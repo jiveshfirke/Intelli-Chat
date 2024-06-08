42fd2249b143bd022dc0110f8a8002d6b5b60aea
brijeshwargt <brijeshwar3012gt@gmail.com>
2024-06-08 15:02:52 +0530

    login screen still need inside content, loginScreenB removed

--- app/src/main/java/com/dedsec/intellichat/screens/login_screen.kt
+++ app/src/main/java/com/dedsec/intellichat/screens/login_screen.kt
@@ -4,10 +4,12 @@ import android.util.Log
 import android.widget.Toast
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.rememberScrollState
@@ -25,6 +27,7 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.SpanStyle
@@ -44,7 +47,6 @@ fun LoginScreen(
     navHostController: NavHostController,
     vm: viewModel
 ){
-
     val context = LocalContext.current
 
     var email by remember {
@@ -54,7 +56,6 @@ fun LoginScreen(
     var password by remember {
         mutableStateOf("")
     }
-
 
     fun checkForInformation(email: String, password: String) {
         if (email.isEmpty() || password.isEmpty()){
@@ -70,15 +71,20 @@ fun LoginScreen(
         }
     }
 
-
     Box(
-        modifier = Modifier
-            .fillMaxSize()
-            .verticalScroll(rememberScrollState()),
+        modifier = Modifier.fillMaxSize(),
         contentAlignment = Alignment.Center
     ){
+        Image(
+            painter = painterResource(id = R.drawable.login_screen_background),
+            contentDescription = "",
+            contentScale = ContentScale.Crop,
+            modifier = Modifier.fillMaxSize()
+            )
         Column(
-            horizontalAlignment = Alignment.CenterHorizontally
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center,
+            modifier = Modifier.fillMaxSize()
         ){
             Image(
                 painter = painterResource(id = R.drawable.iconimg),
