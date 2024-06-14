a424365be614d10ed1215a742f883df530f41269
brijeshwargt <brijeshwar3012gt@gmail.com>
2024-06-14 23:32:45 +0530

    updated color.kt file with shades of red color

--- app/src/main/java/com/dedsec/intellichat/ui/theme/Color.kt
+++ app/src/main/java/com/dedsec/intellichat/ui/theme/Color.kt
@@ -9,3 +9,8 @@ val Pink80 = Color(0xFFEFB8C8)
 val Purple40 = Color(0xFF6650a4)
 val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
+
+val RedDark = Color(0xFFE83232)
+val RedNormal = Color(0xFFF34F4F)
+val RedNormal2 = Color(0xFFFF4545)
+val RedLight = Color(0xFFFF7373)
